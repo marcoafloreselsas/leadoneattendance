560250b31ed96e37e13841aad2d12718dfa0f0bd
marcofloresas <marco.flores@leadonemx.com>
2022-03-15 13:46:43 -0500

    mejoras al login

--- lib/screens/loginpage_screen.dart
+++ lib/screens/loginpage_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:leadoneattendance/screens/mainpage_screen.dart';
 import 'package:leadoneattendance/services/firebase_services.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:leadoneattendance/themes/app_themes.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,7 +12,14 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppTheme.background,
       body: Center(
-        child: SizedBox(
+        child: Column(
+          children: [
+            const SizedBox(height: 250),
+            Image.asset('assets/leadone_logo.png', width: 300,),
+            const SizedBox(height: 20),
+            const Text('loginpage.title').tr(),
+            const SizedBox(height: 20,),
+        SizedBox(
           height: 50,
           width: 180,
           child: SignInButton(
@@ -23,6 +31,8 @@ class LoginPage extends StatelessWidget {
           }
           ),
         ),
+          ],
+        )
      )
     );
   }
