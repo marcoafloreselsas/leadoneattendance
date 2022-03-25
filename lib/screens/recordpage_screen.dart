cb84f4020b84426a66c67f390092dfffc387afde
marcofloresas <marco.flores@leadonemx.com>
2022-03-25 17:18:24 -0500

    respaldo de la semana 2125 de marzo

--- lib/screens/recordpage_screen.dart
+++ lib/screens/recordpage_screen.dart
@@ -72,13 +72,14 @@ class RecordPageScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              //ESTE ES UN COMENTARIO
               ListTile(
                 title: Text("recordpage.TRpermit".tr()),
                 trailing: Wrap(
                   spacing: 12, // space between two icons
                   children: const <Widget>[
                     Icon(Icons.arrow_upward_outlined, color: AppTheme.green,), // icon-1
-                    Text('09:30', style: TextStyle(fontSize: 18),),
+                    Text('08:30', style: TextStyle(fontSize: 18),),
                     Icon(Icons.arrow_downward_outlined, color: AppTheme.red,), // icon-2
                     Text('17:30', style: TextStyle(fontSize: 18),)
                   ],
